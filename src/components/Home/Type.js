97a6f0cfd5ace7add2187faf2d459934499f36ef
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2025-01-19 22:48:33 +0200

    initial release

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Data Scientist",
+          "Data Analyst",
+          "AI Engineer",
+          "Mathematician",
         ],
         autoStart: true,
         loop: true,
