1ce448fa3887dbc435869c7d0066c57f2c9ba640
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2025-01-20 11:52:30 +0200

    fix project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -28,7 +28,7 @@ function Projects() {
               isBlog={false}
               title="Wuzzuf.net Job Postings Dataset"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              kLink="https://www.kaggle.com/datasets/ahmedosamamath/wuzzuf-dataset"
+              kaggleLink="https://www.kaggle.com/datasets/ahmedosamamath/wuzzuf-dataset"
             />
           </Col>
         </Row>
