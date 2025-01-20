c6f3c25e8b31d23c9797a506d2983f947d4aa7ca
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2025-01-20 07:56:41 +0200

    try claude edit

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,113 +2,87 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { FaRProject } from "react-icons/fa";
 import { RiFileExcel2Fill } from "react-icons/ri";
-import {
-  DiPython,
-  DiJavascript1,
-  DiPhp,
-
-  DiMongodb,
-} from "react-icons/di";
+import { DiPython, DiJavascript1, DiPhp, DiMongodb } from "react-icons/di";
 import {
   SiMysql,
   SiPostgresql,
-
   SiPandas,
   SiNumpy,
-
   SiPlotly,
   SiTableau,
-
   SiScikitlearn,
   SiScipy,
   SiKeras,
   SiTensorflow,
   SiPytorch,
-
   SiStreamlit,
   SiFlask,
   SiDash,
-
   SiApachespark,
   SiApachehadoop,
 } from "react-icons/si";
 
+const TechCategory = ({ title, children }) => (
+  <div className="mb-8">
+    <h3 className="text-xl font-bold mb-4">{title}</h3>
+    <Row className="justify-center pb-4">
+      {children}
+    </Row>
+  </div>
+);
+
+const TechIcon = ({ children }) => (
+  <Col xs={4} md={2} className="tech-icons">
+    {children}
+  </Col>
+);
+
 function Techstack() {
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <FaRProject />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPhp />
-      </Col>
+    <div className="p-4">
+      <TechCategory title="Programming Languages">
+        <TechIcon><DiPython /></TechIcon>
+        <TechIcon><FaRProject /></TechIcon>
+        <TechIcon><DiJavascript1 /></TechIcon>
+        <TechIcon><DiPhp /></TechIcon>
+      </TechCategory>
 
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
+      <TechCategory title="Databases">
+        <TechIcon><SiMysql /></TechIcon>
+        <TechIcon><SiPostgresql /></TechIcon>
+        <TechIcon><DiMongodb /></TechIcon>
+      </TechCategory>
 
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPandas />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNumpy />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <RiFileExcel2Fill />
-      </Col>
+      <TechCategory title="Data Processing & Analysis">
+        <TechIcon><SiPandas /></TechIcon>
+        <TechIcon><SiNumpy /></TechIcon>
+        <TechIcon><RiFileExcel2Fill /></TechIcon>
+      </TechCategory>
 
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPlotly />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTableau />
-      </Col>
-     
-      <Col xs={4} md={2} className="tech-icons">
-        <SiScikitlearn />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiScipy />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiKeras />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiTensorflow />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPytorch />
-      </Col>
+      <TechCategory title="Data Visualization">
+        <TechIcon><SiPlotly /></TechIcon>
+        <TechIcon><SiTableau /></TechIcon>
+      </TechCategory>
 
-      <Col xs={4} md={2} className="tech-icons">
-        <SiStreamlit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFlask />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiDash />
-      </Col>
-      
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachespark />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachehadoop />
-      </Col>
-    </Row>
+      <TechCategory title="Machine Learning & AI">
+        <TechIcon><SiScikitlearn /></TechIcon>
+        <TechIcon><SiScipy /></TechIcon>
+        <TechIcon><SiKeras /></TechIcon>
+        <TechIcon><SiTensorflow /></TechIcon>
+        <TechIcon><SiPytorch /></TechIcon>
+      </TechCategory>
+
+      <TechCategory title="Web Frameworks">
+        <TechIcon><SiStreamlit /></TechIcon>
+        <TechIcon><SiFlask /></TechIcon>
+        <TechIcon><SiDash /></TechIcon>
+      </TechCategory>
+
+      <TechCategory title="Big Data">
+        <TechIcon><SiApachespark /></TechIcon>
+        <TechIcon><SiApachehadoop /></TechIcon>
+      </TechCategory>
+    </div>
   );
 }
 
