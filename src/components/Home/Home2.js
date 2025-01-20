7ff7cca713b5883b1dee849b377808e0ac5b386a
Ahmed Osama <ahmedosamabusiness0@gmail.com>
2025-01-20 12:18:26 +0200

    finish home page

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -21,37 +21,21 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Javascript and Go. </b>
-              </i>
+              I fell in love with programming and data science, and I have learned a lot along the way, constantly striving to grow and improve.
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+              I am fluent in languages like 
+              <i><b className="purple">Python and SQL</b></i>, with experience in libraries such as 
+              <i><b className="purple">Pandas, NumPy, and TensorFlow</b></i>.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              My field of interest lies in 
+              <i><b className="purple">applying data science</b></i> to solve real-world problems, 
+              <i><b className="purple">building machine learning models</b></i>, and 
+              <i><b className="purple">exploring the potential of AI.</b></i>
+              <br />
+              <br />
+              Whenever possible, I apply my passion for data by <i><b className="purple">developing insightful visualizations</b></i>, <i><b className="purple">creating predictive models</b></i>, and leveraging advanced techniques in machine learning and deep learning to turn raw data into <i><b className="purple">actionable insights.</b></i>
             </p>
           </Col>
           <Col md={4} className="myAvtar">
